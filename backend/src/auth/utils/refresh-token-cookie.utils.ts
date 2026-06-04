ef6bac830933f9f1d6f0621f3a462350b934fb46
Noam Shimony <83021190+ObamaCooking@users.noreply.github.com>
2026-06-04 16:46:02 +0300

    feat(refresh-token-cookie): Add cookie configuration

---
+++ backend/src/auth/utils/refresh-token-cookie.utils.ts
@@ -0,0 +1,23 @@
+import { CookieOptions, Response } from 'express';
+import ms from 'ms';
+import { env } from 'src/config/env';
+
+const REFRESH_TOKEN_COOKIE_NAME = 'refresh_token';
+
+const REFRESH_TOKEN_COOKIE_OPTIONS = {
+    httpOnly: true,
+    path: '/auth/refresh',
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'lax',
+} as CookieOptions;
+
+export const setRefreshTokenCookie = (res: Response, token: string): void => {
+    res.cookie(REFRESH_TOKEN_COOKIE_NAME, token, {
+        ...REFRESH_TOKEN_COOKIE_OPTIONS,
+        maxAge: ms(env.REFRESH_TOKEN_EXPIRES_IN),
+    });
+};
+
+export const clearRefreshTokenCookie = (res: Response): void => {
+    res.clearCookie(REFRESH_TOKEN_COOKIE_NAME, REFRESH_TOKEN_COOKIE_OPTIONS);
+};
