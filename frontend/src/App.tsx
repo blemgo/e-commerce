3c4039543ed2c48805d0bdfe448e1e9f04c4ba9e
Noam Shimony <83021190+ObamaCooking@users.noreply.github.com>
2026-06-07 16:09:40 +0300

    feat(user): Use UserProvider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,15 @@ import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { Router } from '@/router';
 import theme from './theme/theme';
+import { UserProvider } from '@contexts/user';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Router />
+      <UserProvider>
+        <Router />
+      </UserProvider>
     </ThemeProvider>
   );
 };
