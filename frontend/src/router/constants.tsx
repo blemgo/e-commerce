b1798e63da571a3c70f34a70896b03b2f8ca8475
Noam Shimony <83021190+ObamaCooking@users.noreply.github.com>
2026-06-09 23:59:01 +0300

    feat(constants): Add productPage

--- frontend/src/router/constants.tsx
+++ frontend/src/router/constants.tsx
@@ -1,11 +1,13 @@
 import { Home } from '@views/Home';
 import { CatalogPage } from '@views/Catalog';
+import { ProductPage } from '@views/ProductPage';
 import { LoginPage } from '@views/Login';
 import type { Page } from './types';
 
 export const PAGES: Page[] = [
   { path: '/', element: <Home />, name: 'Home' },
   { path: '/catalog', element: <CatalogPage />, name: 'Catalog' },
+  { path: '/product/:productId', element: <ProductPage />, name: 'Product' },
 ];
 
 export const AUTH_PAGES: Page[] = [
