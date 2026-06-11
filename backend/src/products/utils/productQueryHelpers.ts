6aed296919bd1b90c16e36f89e07dcb04cb6c98b
Noam Shimony <83021190+ObamaCooking@users.noreply.github.com>
2026-06-11 20:00:23 +0300

    feat(PaginatedResult): Introduce PaginatedResult interface and refactor PaginatedProducts type to use it

--- backend/src/products/utils/productQueryHelpers.ts
+++ backend/src/products/utils/productQueryHelpers.ts
@@ -1,13 +1,9 @@
 import { SelectQueryBuilder } from 'typeorm';
 import { Product } from '../entities/product.entity';
 import { GetProductsQueryDto } from '../dto/get-products-query.dto';
+import { PaginatedResult } from 'src/common/interfaces/paginated-result.interface';
 
-export interface PaginatedProducts {
-  data: Product[];
-  totalCount: number;
-  page: number;
-  totalPages: number;
-}
+export type PaginatedProducts = PaginatedResult<Product>;
 
 export const applyQuery = (
   qb: SelectQueryBuilder<Product>,
