6bbeed4c94c2ab213bb5efe12db6549411657d6d
Noam Shimony <83021190+ObamaCooking@users.noreply.github.com>
2026-06-13 18:01:15 +0300

    del(many): Remove unused components

--- frontend/src/components/CategoryBar/CategoryBar.styles.ts
+++
@@ -1,9 +0,0 @@
-import type { SxProps, Theme } from '@mui/material';
-
-export const categoryBarStyles: Record<string, SxProps<Theme>> = {
-  tabs: {
-    mb: 2,
-    borderBottom: 1,
-    borderColor: 'divider',
-  },
-};
